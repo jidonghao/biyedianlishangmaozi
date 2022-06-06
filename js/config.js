24e9bd3e2d8f8318000aa4295ac326cd9cf522bc
jdh001 <ji534@outlook.com>
2022-06-06 11:02:10 +0800

    新增config 名单

---
+++ js/config.js
@@ -0,0 +1,21 @@
+
+const config = {
+    filePath : "img/",  //这里是图片存放的文件夹 
+    numOfPerson : 3, //人数
+    // 
+    List:{
+        "1": {
+            "name": "姓名1",
+            "path": "person.jpg"
+        },
+        "2": {
+            "name": "姓名2",
+            "path": "person1.jpg"
+        },
+        "3": {
+            "name": "姓名3",
+            "path": "person2.jpg"
+        },
+    }
+    
+}
