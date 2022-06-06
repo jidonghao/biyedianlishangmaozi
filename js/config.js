d9cf664b339c156631fd3d915f4b55bd892f0534
jdh001 <ji534@outlook.com>
2022-06-06 11:07:08 +0800

    改成数组

--- js/config.js
+++ js/config.js
@@ -1,21 +1,20 @@
 
 const config = {
     filePath : "img/",  //这里是图片存放的文件夹 
-    numOfPerson : 3, //人数
     // 
-    List:{
-        "1": {
+    List :[
+         {
             "name": "姓名1",
             "path": "person.jpg"
         },
-        "2": {
+         {
             "name": "姓名2",
             "path": "person1.jpg"
         },
-        "3": {
+         {
             "name": "姓名3",
             "path": "person2.jpg"
         },
-    }
+    ]
     
 }
