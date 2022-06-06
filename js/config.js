802d1a0eff85744840519761aff4c6f87f44f42e
jidonghao <ji534@outlook.com>
2022-06-06 19:10:51 +0800

    调整边距

--- js/config.js
+++ js/config.js
@@ -2,25 +2,23 @@ const config = {
     filePath: "img/", //这里是图片存放的文件夹 
     // 
     List: [
-       
-        {"name":"姓名1","path":"person.jpg"},
-        {"name":"姓名2","path":"person1.jpg"},
-        {"name":"姓名3","path":"person2.jpg"},
-        {"name":"姓名4","path":"person.jpg"},
-        {"name":"姓名5","path":"person1.jpg"},
-        {"name":"姓名6","path":"person2.jpg"},
-        {"name":"姓名7","path":"person.jpg"},
-        {"name":"姓名8","path":"person1.jpg"},
-        {"name":"姓名9","path":"person2.jpg"},
-        {"name":"姓名10","path":"person.jpg"},
-        {"name":"姓名11","path":"person1.jpg"},
-        {"name":"姓名12","path":"person2.jpg"},
-        {"name":"姓名13","path":"person.jpg"},
-        {"name":"姓名14","path":"person1.jpg"},
-        {"name":"姓名15","path":"person2.jpg"},
-        {"name":"姓名16","path":"person.jpg"},
-        {"name":"姓名17","path":"person1.jpg"},
-        
+        { "name": "姓名1", "path": "person.jpg" },
+        { "name": "姓名2", "path": "person1.jpg" },
+        { "name": "姓名3", "path": "person2.jpg" },
+        { "name": "姓名4", "path": "person.jpg" },
+        { "name": "姓名5", "path": "person3.jpg" },
+        { "name": "姓名6", "path": "person4.jpg" },
+        { "name": "姓名7", "path": "person.jpg" },
+        { "name": "姓名8", "path": "person5.jpg" },
+        { "name": "姓名9", "path": "person6.jpg" },
+        { "name": "姓名10", "path": "person.jpg" },
+        { "name": "姓名11", "path": "person7.jpg" },
+        { "name": "姓名12", "path": "person8.jpg" },
+        { "name": "姓名13", "path": "person.jpg" },
+        { "name": "姓名14", "path": "person9.jpg" },
+        { "name": "姓名15", "path": "person10.jpg" },
+        { "name": "姓名16", "path": "person.jpg" },
+        { "name": "姓名17", "path": "person11.jpg" },
 
     ]
 
