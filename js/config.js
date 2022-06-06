409509ce01fd74f6e8efbbeb1ed435ab83faf12f
jidonghao <ji534@outlook.com>
2022-06-06 13:39:38 +0800

    大致完成

--- js/config.js
+++ js/config.js
@@ -1,20 +1,68 @@
-
 const config = {
-    filePath : "img/",  //这里是图片存放的文件夹 
+    filePath: "img/", //这里是图片存放的文件夹 
     // 
-    List :[
-         {
+    List: [{
             "name": "姓名1",
             "path": "person.jpg"
         },
-         {
+        {
             "name": "姓名2",
             "path": "person1.jpg"
         },
-         {
+        {
+            "name": "姓名3",
+            "path": "person2.jpg"
+        },
+        {
+            "name": "姓名1",
+            "path": "person.jpg"
+        },
+        {
+            "name": "姓名2",
+            "path": "person1.jpg"
+        },
+        {
+            "name": "姓名3",
+            "path": "person2.jpg"
+        },
+        {
+            "name": "姓名1",
+            "path": "person.jpg"
+        },
+        {
+            "name": "姓名2",
+            "path": "person1.jpg"
+        },
+        {
+            "name": "姓名3",
+            "path": "person2.jpg"
+        }, {
+            "name": "姓名1",
+            "path": "person.jpg"
+        },
+        {
+            "name": "姓名2",
+            "path": "person1.jpg"
+        },
+        {
+            "name": "姓名3",
+            "path": "person2.jpg"
+        }, {
+            "name": "姓名1",
+            "path": "person.jpg"
+        },
+        {
+            "name": "姓名2",
+            "path": "person1.jpg"
+        },
+        {
+            "name": "姓名3",
+            "path": "person2.jpg"
+        },
+        {
             "name": "姓名3",
             "path": "person2.jpg"
         },
     ]
-    
+
 }
--- js/index.js
+++ js/index.js
@@ -7,11 +7,64 @@ let $maozi = document.querySelectorAll(".maozi")
 //     $maozi[i].style.display = "none"
 //     // $maozi[i].style.animationDelay = Number(1.5 * i) + "s"
 // }
-let j = 0
-var timer = setInterval(() => {
-    $maozi[j++].style.display = "block"
-    if (j >= $maozi.length) {
-        clearInterval(timer)
-        j = 0
+
+
+function delayShow() {
+    let j = 0
+    var timer = setInterval(() => {
+        $maozi[j++].style.display = "block"
+        if (j >= $maozi.length) {
+            clearInterval(timer)
+            j = 0
+        }
+    }, 1500);
+}
+delayShow()
+
+const imgimg = document.querySelectorAll(".showImgName .imgimg")
+const namename = document.querySelectorAll(".showImgName .namename")
+
+const app = document.querySelector(".app")
+
+let length = config.List.length
+
+let showImgDiv = document.querySelectorAll(".showImgName")
+
+
+
+for (let j = 0; j < 8; j++) {
+    imgimg[j].src = config.filePath + config.List[j].path
+    namename[j].innerHTML = config.List[j].name
+    showImgDiv[j].style.display = "inline-block"
+}
+
+let i = 7
+let timer = setInterval(() => {
+    for (let j = 0; j < 8; j++) {
+        showImgDiv[j].style.display = "none"
     }
-}, 1500);
+
+    // 刷新一下页面开始切换下一页
+    console.log("切换下一页")
+    app.style.display = "none"
+    for (let o = 0; o < $maozi.length; o++) {
+        $maozi[o].style.display = "none"
+    }
+    app.style.display = "block"
+    delayShow()
+
+    if (i >= length)
+        clearInterval(timer)
+    console.log("定时器开始执行")
+    for (let j = 0; j < 8; j++) {
+        if ((j + i) >= length) {
+            clearInterval(timer)
+        } else {
+
+            imgimg[j].src = config.filePath + config.List[i + j].path
+            namename[j].innerHTML = config.List[i + j].name
+            showImgDiv[j].style.display = "inline-block"
+        }
+    }
+    i += 8
+}, 15000);
