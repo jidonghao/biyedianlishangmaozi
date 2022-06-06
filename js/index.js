d3b149ca3c935e18375b0423c2f5f092fad22c22
jdh001 <ji534@outlook.com>
2022-06-06 16:01:04 +0800

    压缩文件

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('1 $5=7.b(".5")z k(){1 j=0 A a=s(()=>{$5[j++].2.3="c"l(j>=$5.6){m(a)j=0}},B)}k()n d=7.b(".p .d")n e=7.b(".p .e")n f=7.C(".f")1 6=4.9.6 1 g=7.b(".p")h(1 j=0;j<8;j++){d[j].t=4.u+4.9[j].v e[j].w=4.9[j].x g[j].2.3="y-c"}1 i=8 1 q=s(()=>{h(1 j=0;j<8;j++){g[j].2.3="r"}f.2.3="r"h(1 o=0;o<$5.6;o++){$5[o].2.3="r"}f.2.3="c"k()l(i>=6)m(q)h(1 j=0;j<8;j++){l((j+i)>=6){m(q)}D{d[j].t=4.u+4.9[i+j].v e[j].w=4.9[i+j].x g[j].2.3="y-c"}}i+=8},E);',41,41,'|let|style|display|config|maozi|length|document||List||querySelectorAll|block|imgimg|namename|app|showImgDiv|for|||delayShow|if|clearInterval|const||showImgName|timer|none|setInterval|src|filePath|path|innerHTML|name|inline|function|var|1500|querySelector|else|15000'.split('|'),0,{}))
+"use strict";
+
+function delayShow() {
+    var e = 0, i = setInterval(function() {
+        $maozi[e++].style.display = "block", e >= $maozi.length && (clearInterval(i), e = 0);
+    }, 1500);
+}
+
+var $maozi = document.querySelectorAll(".maozi");
+
+delayShow();
+
+for (var imgimg = document.querySelectorAll(".showImgName .imgimg"), namename = document.querySelectorAll(".showImgName .namename"), app = document.querySelector(".app"), length = config.List.length, showImgDiv = document.querySelectorAll(".showImgName"), j = 0; j < 8; j++) imgimg[j].src = config.filePath + config.List[j].path, 
+namename[j].innerHTML = config.List[j].name, showImgDiv[j].style.display = "inline-block";
+
+var i = 8, timer = setInterval(function() {
+    for (var e = 0; e < 8; e++) showImgDiv[e].style.display = "none";
+    app.style.display = "none";
+    for (var n = 0; n < $maozi.length; n++) $maozi[n].style.display = "none";
+    app.style.display = "block", delayShow(), i >= length && clearInterval(timer);
+    for (var a = 0; a < 8; a++) a + i >= length ? clearInterval(timer) : (imgimg[a].src = config.filePath + config.List[i + a].path, 
+    namename[a].innerHTML = config.List[i + a].name, showImgDiv[a].style.display = "inline-block");
+    i += 8;
+}, 15e3);
